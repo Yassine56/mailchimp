26060d93096ee5710138da2c504049e5cd0bca8d
Abouelouafa Yassine <abouelouafayassine@gmail.com>
2018-11-09 11:00:06 -0500

    prod keys

--- config/prod.js
+++ config/prod.js
@@ -6,5 +6,9 @@ module.exports = {
   cookieKey : process.env.COOKIE_KEY,
   stripePublishableKey : process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey : process.env.STRIPE_SECRET_KEY,
-  sendGridApiKey : process.env.SENDGRID_API_KEY
+  sendGridApiKey : process.env.SENDGRID_API_KEY,
+  facebookClientSecret : process.env.FB_CLIENT_SECRET,
+  facebookClientID : process.env.FB_CLIENT_ID,
+  githubClientID: process.env.GITHUB_CLIENT_ID,
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET
 };
